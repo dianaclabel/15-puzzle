5ce9b416bea1b95e052658061aab79b179b6e6f7
Diana Clabel <dianaclabel08@gmail.com>
2024-04-12 19:53:24 -0500

    Estructura y estilos

---
+++ main.js
@@ -0,0 +1,23 @@
+// funcion para mover las box
+// funcion para barajear la matriz
+// crear una matriz
+
+let divBox = document.querySelectorAll("box");
+
+let matrizPosicion = [
+  ["0", "0"],
+  ["0", "175"],
+  ["0", "350"],
+  ["0", "525"],
+  ["175", "0"],
+  ["175", "175"],
+  ["175", "350"],
+  ["175", "525"],
+  ["350", "0"],
+  ["350", "175"],
+  ["350", "350"],
+  ["350", "525"],
+  ["525", "0"],
+  ["525", "175"],
+  ["525", "350"],
+];
